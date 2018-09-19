2c3ee2921a71fc74f14e7c8369464c14aab728a6
Juan Evangelista Fletes Garcia <juanfletes007@gmail.com>
2018-09-18 19:40:39 -0700

    working in locales

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module Hellocrm
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
     config.i18n.default_locale = :es
     config.i18n.available_locales = [:en, :es]
 
